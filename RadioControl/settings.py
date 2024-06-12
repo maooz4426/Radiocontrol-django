fb165e0fdb4fa8c2a43e0dabec62c8b051113c3f
MAOZ <maooz4426@gmail.com>
2024-06-12 18:45:02 +0900

    Procfile修正

--- RadioControl/settings.py
+++ RadioControl/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 
-'whitenoise.middleware.WhiteNoiseMiddleware',#redisサーバー使ってるので必要
+    'whitenoise.middleware.WhiteNoiseMiddleware',#redisサーバー使ってるので必要
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
